acdc035318839c31f1996f31484f93d3f549b992
ypanasyuk <ypanasyuk@cinimex.ru>
2016-02-18 13:11:26 +0300

    single threaded votes counter

--- tests/votes_counter_tests.py
+++ tests/votes_counter_tests.py
@@ -29,112 +29,33 @@ class TestVotesCounter(unittest.TestCase):
         pass
 
     def test_six_vote(self):
-        vote = """
-        {
-            "voter": {
-                "id": "5d7370ded13d"
-            },
-            "answers": [
-                {
-                    "question": {
-                        "title": "Утверждение итогов работы компании",
-                        "files": [
-                            {
-                                "file_title": "Годовойотчёт",
-                                "file_url": "http://www.gazprom.ru/f/posts/05/298369/gazprom-annual-report-2014-ru.pdf",
-                                "file_sha2": "959ca7b22af7725d7370ded13d3a3f53b3b2ff953a3f6267075438e141ee4525"
-                            }
-                        ]
-                    },
-                    "vote": {
-                        "да": false,
-                        "нет": false,
-                        "воздержался": true,
-                        "не голосовал": false
-                    }
-                },
-                {
-                    "question": {
-                        "title": "Выбор нового председателя"
-                    },
-                    "vote": {
-                        "Иванов": 5,
-                        "Сидоров": 1,
-                        "Петров": 6,
-                        "воздержался": false,
-                        "не голосовал": false
-                    }
-                }
-            ]
-        }
-        """
+        with open('resources/journal_template.json','r') as journal_template_file:
+            journal_template = journal_template_file.read()
+        with open('resources/journal_record_template.json','r') as record_template_file:
+            record_template = record_template_file.read()
+        with open('resources/record_vote_template.json','r') as vote_template_file:
+            vote_template = vote_template_file.read()
 
+        vote = vote_template
         voteBase64 = b64encode(vote)
 
-        record = """
-                       {
-                    "id": "b3749f23-d9f2-401e-91ca-673956a79066",
-                    "fingerprints": [
-                        {
-                            "metadata": "%s",
-                            "metadataContentType": "application/json;enc=v1",
-                            "metadataHash": "",
-                            "nonce": ""
-                        }
-                    ],
-                    "timestamps": [
-                        {
-                            "txid": "74dc7d0cadf60bbb1d06a99f41db7b0a4e620d4c66cdc020729796e6fd0b8260",
-                            "proof": {
-                                "sequence": [
-                                    {
-                                        "hash": "EbZNktUdzgMDfwqWFVWigHT7Ca66RqX3wan2seVRP2w=",
-                                        "direction": "RIGHT"
-                                    }
-                                ],
-                                "op_return": "df4fff68d8ce5c0ee72b7fb3f28c41b12da96d50e1f08474133c5b1ab63e71253e0d1d25ceaafaf3",
-                                "root": "K1dJGN8tpUnI4bwLdntgpRjkLnsO/CRcbNy3Y6eAlTo=",
-                                "fingerprint_ripemd160": "30//aNjOXA7nK3+z8oxBsS2pbVA=",
-                                "nonce": "4fCEdBM8Wxq2PnElPg0dJc6q+vM="
-                            },
-                            "fingerprint": "yZxkSpLS8sTyqocIqPbaqFXIDqQ=",
-                            "nonce": "4fCEdBM8Wxq2PnElPg0dJc6q+vM="
-                        }
-                    ],
-                    "files": [],
-                    "fingerprint": "oPRo+QHEkeA2ewxIaOV6WYveGsg=",
-                    "nonce": "nIADOspyC8QnHiC9Nr8PnvP18OY="
-                }
-        """ % voteBase64
+        record = record_template % voteBase64
 
-        records = (record+",")*5+record
+        voters_number = 100000
+        records = (record+",")*(voters_number-1)+record
 
-        journal = """
-        {
-            "id": "5b9a74e9-52e0-4727-82a6-6b7286f6bcd3",
-            "type": "blockchain_merkletree",
-            "name": "Ernesto O'Connell",
-            "records": [
-                %s
-            ],
-            "timestamps": [
-                {
-                    "txid": "74dc7d0cadf60bbb1d06a99f41db7b0a4e620d4c66cdc020729796e6fd0b8260"
-                }
-            ]
-        }
-        """ % records
+        journal = journal_template % records
 
         expected = json.loads("""{
-            "\u0412\u044b\u0431\u043e\u0440 \u043d\u043e\u0432\u043e\u0433\u043e \u043f\u0440\u0435\u0434\u0441\u0435\u0434\u0430\u0442\u0435\u043b\u044f": {
-                "\u0418\u0432\u0430\u043d\u043e\u0432": 30,
-                "\u041f\u0435\u0442\u0440\u043e\u0432": 36,
-                "\u0421\u0438\u0434\u043e\u0440\u043e\u0432": 6,
+            "Выбор нового председателя": {
+                "\u0418\u0432\u0430\u043d\u043e\u0432": """+str(5*voters_number)+""",
+                "\u041f\u0435\u0442\u0440\u043e\u0432": """+str(6*voters_number)+""",
+                "\u0421\u0438\u0434\u043e\u0440\u043e\u0432": """+str(voters_number)+""",
                 "\u0432\u043e\u0437\u0434\u0435\u0440\u0436\u0430\u043b\u0441\u044f": 0,
                 "\u043d\u0435 \u0433\u043e\u043b\u043e\u0441\u043e\u0432\u0430\u043b": 0
             },
             "\u0423\u0442\u0432\u0435\u0440\u0436\u0434\u0435\u043d\u0438\u0435 \u0438\u0442\u043e\u0433\u043e\u0432 \u0440\u0430\u0431\u043e\u0442\u044b \u043a\u043e\u043c\u043f\u0430\u043d\u0438\u0438": {
-                "\u0432\u043e\u0437\u0434\u0435\u0440\u0436\u0430\u043b\u0441\u044f": 6,
+                "\u0432\u043e\u0437\u0434\u0435\u0440\u0436\u0430\u043b\u0441\u044f": """+str(voters_number)+""",
                 "\u0434\u0430": 0,
                 "\u043d\u0435 \u0433\u043e\u043b\u043e\u0441\u043e\u0432\u0430\u043b": 0,
                 "\u043d\u0435\u0442": 0
--- utils/votes_counter.py
+++ utils/votes_counter.py
@@ -4,6 +4,8 @@
 
 import json
 import base64
+import time
+import multiprocessing
 
 class VotesCounter:
     def __init__(self):
@@ -14,29 +16,23 @@ class VotesCounter:
         # May be should use Pool.map for this
 
         votes = list()
-
-        journal = json.loads(journalString);
-        for record in journal['records']:
-            for fingerprint in record['fingerprints']:
-                votes.append(
-                    json.loads(
-                        base64.b64decode(fingerprint['metadata'])
-                    )
-                )
-
         results = {}
 
-        for vote in votes:
-            for answer in vote['answers']:
-                question_title = answer['question']['title']
-                question_result = results.get(question_title,None)
-                if (question_result == None): question_result = {}; results[question_title]=question_result;
-                for key, answer in answer['vote'].items():
-                    question_result[key]=question_result.get(key,0)+answer
+        journal = json.loads(journalString);
+        del journalString
 
+        # Sequence processing
+        for record in journal['records']:
+            for fingerprint in record['fingerprints']:
+                vote = json.loads(base64.b64decode(fingerprint['metadata']))
+                for answer in vote['answers']:
+                    question_title = answer['question']['title']
+                    question_result = results.get(question_title,None)
+                    if (question_result == None): question_result = {}; results[question_title]=question_result;
+                    for key, answer in answer['vote'].items():
+                        question_result[key]=question_result.get(key,0)+answer
+        del journal
 
-        print(json.dumps(results, sort_keys=True, indent=4, separators=(',',': ')))
-
-        result = {'txid': '74dc7d0cadf60bbb1d06a99f41db7b0a4e620d4c66cdc020729796e6fd0b8260'}
+        print(json.dumps(results, sort_keys=True, encoding="UTF-8", indent=4, separators=(',',': ')))
 
         return results
