4e83f077a8f79baf5881810576d25e34d99b3a50
ypanasyuk <ypanasyuk@cinimex.ru>
2016-02-18 13:34:59 +0300

    single threaded votes counter

--- tests/votes_counter_tests.py
+++ tests/votes_counter_tests.py
@@ -29,11 +29,11 @@ class TestVotesCounter(unittest.TestCase):
         pass
 
     def test_six_vote(self):
-        with open('resources/journal_template.json','r') as journal_template_file:
+        with open('tests/resources/journal_template.json','r') as journal_template_file:
             journal_template = journal_template_file.read()
-        with open('resources/journal_record_template.json','r') as record_template_file:
+        with open('tests/resources/journal_record_template.json','r') as record_template_file:
             record_template = record_template_file.read()
-        with open('resources/record_vote_template.json','r') as vote_template_file:
+        with open('tests/resources/record_vote_template.json','r') as vote_template_file:
             vote_template = vote_template_file.read()
 
         vote = vote_template
