89d13c709313b04f0b5fa2f1a79f94350e701737
Vladimir Buzov <vladimir.buzov@gmail.com>
2016-02-18 21:21:48 +0300

    Added new params saving to setup_ledger

--- setup_ledger.py
+++ setup_ledger.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # Author: Igor Barinov <igorbarinov@me.com>
 
+import sys, os
 import requests
+import logging
+
 from requests.auth import HTTPBasicAuth
 from ConfigParser import SafeConfigParser
 from faker import Factory
 from base64 import b64encode
-import sys, os
+from utils.ledger_api import LedgerApi
 
 # setup loggging
-import logging
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 faker = Factory.create()
 
 parser = SafeConfigParser()
 parser.read('load.ini')
-AL_HOST = parser.get('general', 'host')
-headers = {'Content-Type':'application/json'}
+
+baseUrl = parser.get('general', 'host')
+
+ledgerApi = LedgerApi(baseUrl)
 
 """
 
@@ -31,18 +36,27 @@ Create user
 # @click.command()
 # @click.option('--timestamp-weight', default=1, help='Weight of a timestamp operation')
 
+logger.debug('Starting creating an user')
 
-def create_user():
-    r = requests.post(AL_HOST + '/api/users', headers= headers, data =  '{"email": "' + faker.email() + '"}')
-    key = str(r.json()["accessTokens"][0]["accessKey"])
-    secret = str(r.json()["accessTokens"][0]["accessSecret"])
-    userAndPass = "Basic " + b64encode(key + ":" + secret).decode("ascii")
-    return userAndPass
-logger.debug('Starting creating user')
+username = faker.email()
+password = "password"
 
-AL_AUTH = str(create_user())
+AL_AUTH = ledgerApi.createUser(username, password)
+
+# save user info to config
 parser.set('general', 'auth', AL_AUTH)
-logger.debug('User created')
+parser.set('general', 'username', username)
+parser.set('general', 'password', password)
+
+logger.debug('User created.')
+
+"""
+
+Create journal
+
+"""
+
+logger.debug('Starting creating a journal')
 
 def create_journal():
     payload = '{"name": "' + faker.name() + '", "type": "blockchain_merkletree"}'
@@ -50,8 +64,13 @@ def create_journal():
     r = requests.post(AL_HOST + '/api/journals', headers = headers, data = payload)
     return r.json()["id"]
 
-AL_JOURNAL = str(create_journal())
+AL_JOURNAL = ledgerApi.createJournal(AL_AUTH, faker.name())
+
+# save journal info to cinfig
 parser.set('general', 'journal', AL_JOURNAL)
 
-with open('load.ini', 'w') as configfile:    # save
+logger.debug('Journal created.')
+
+# Saving configuration
+with open('load.ini', 'w') as configfile:
     parser.write(configfile)
